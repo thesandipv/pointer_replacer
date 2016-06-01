2887b2e13370d8c838aa43708c98f81189e3a4c9
sandipv22 <sandipvaghela311@gmail.com>
2016-06-01 16:15:28 +0530

    Added new apk download method

--- app/src/main/java/afterroot/pointerreplacer/UpdateActivity.java
+++ app/src/main/java/afterroot/pointerreplacer/UpdateActivity.java
@@ -27,6 +27,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -34,7 +35,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -106,7 +106,6 @@ public class UpdateActivity extends AppCompatActivity {
     }
 
     private void checkForUpdate() {
-        Toast.makeText(this, "Please Wait...", Toast.LENGTH_SHORT).show();
         setupUpdater();
         cardView_update.setVisibility(View.VISIBLE);
     }
@@ -134,11 +133,12 @@ public class UpdateActivity extends AppCompatActivity {
                 String changelog = dlString(UpdateURLs.URL_CHANGELOG, true);
                 textNewChangelog.setText(changelog);
 
+                final String apkURL = dlString(UpdateURLs.URL_DOWNLOAD_APK, false);
+
                 buttonUpdate.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         final String apkName = "PR_"+newVersionName+".apk";
-                        String apkURL = "https://raw.githubusercontent.com/sandipv22/pointer_replacer/main/updater/"+apkName;
                         final File downlaodedApk = new File(Environment.getExternalStorageDirectory()+"/Pointer Replacer/Downloads/"+apkName);
                         if (downlaodedApk.exists()){
                             showSnackbar(findViewById(R.id.main_layoutUpdate), apkName+" already exists.");
@@ -168,6 +168,8 @@ public class UpdateActivity extends AppCompatActivity {
             showSnackbar(findViewById(R.id.main_layoutUpdate), "No Connection");
         }
     }
+
+    @NonNull
     private static String convertStreamToString(InputStream inputStream) throws UnsupportedEncodingException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
         StringBuilder sb = new StringBuilder();
@@ -190,11 +192,8 @@ public class UpdateActivity extends AppCompatActivity {
 
     private boolean isNetworkAvailable(){
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkWiFi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo networkMobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-        return networkWiFi != null && networkWiFi.isAvailable() && networkWiFi.isConnectedOrConnecting() ||
-                networkMobile != null && networkMobile.isAvailable() && networkMobile.isConnectedOrConnecting();
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 
     private void openFile(String filname, Uri downloadedFile){
@@ -206,13 +205,12 @@ public class UpdateActivity extends AppCompatActivity {
     private static String dlString(String url, boolean fetchLines) {
         String dS = "";
         try {
-            StringAsync.setFetchLines(fetchLines);
-            StringAsync stringAsync = new StringAsync ();
-            dS = stringAsync.execute(new String[] {url}).get();
+            DownloadTxtTask.setFetchLines(fetchLines);
+            DownloadTxtTask DownloadTxtTask = new DownloadTxtTask ();
+            dS = DownloadTxtTask.execute(new String[] {url}).get();
         } catch(Exception ex) {
             //
         }
-
         return dS;
     }
 
@@ -220,7 +218,7 @@ public class UpdateActivity extends AppCompatActivity {
         setup();
     }
 
-    private static class StringAsync extends AsyncTask< String, Integer, String > {
+    private static class DownloadTxtTask extends AsyncTask< String, Integer, String > {
         static boolean isFetchLines;
 
         static void setFetchLines(boolean fetchLines){
@@ -264,7 +262,8 @@ public class UpdateActivity extends AppCompatActivity {
                 URL_REPO = URL_GITHUB + "sandipv22/pointer_replacer/main/version_checker/",
                 URL_VERSION_CODE = URL_REPO + "version_code.txt",
                 URL_VERSION_NAME = URL_REPO + "version_name.txt",
-                URL_CHANGELOG = URL_REPO + "changelog.txt";
+                URL_CHANGELOG = URL_REPO + "changelog.txt",
+                URL_DOWNLOAD_APK = URL_REPO + "download_url.txt";
 
     }
 }
