81048632d5bc3725c2929f3a6b77c16378cac632
Sandip <sandipvaghela311@gmail.com>
2019-02-03 13:23:14 +0530

    Added Firebase Cloud Notifications

---
+++ touchesenabler/src/main/java/com/afterroot/touchenabler/FireMessagingService.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2016-2019 Sandip Vaghela
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.afterroot.touchenabler
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FireMessagingService : FirebaseMessagingService() {
+
+    private val TAG = "FireMessagingService"
+    private val keyUrl = "FCM_URL"
+
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+        Log.e(TAG, "NEW_TOKEN $token")
+
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            val url = remoteMessage.data[keyUrl]
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+        }
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, "ShowTouches")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(remoteMessage.notification!!.body)
+                .setAutoCancel(false)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
