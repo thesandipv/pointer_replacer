bda06618ac5e119e7acc7be30e72fc33be899fbd
Sandip <sandipvaghela311@gmail.com>
2019-02-21 22:48:19 +0530

    touchesenabler: added splash screen

--- touchesenabler/src/main/java/com/afterroot/touchenabler/MainActivity.kt
+++ touchesenabler/src/main/java/com/afterroot/touchenabler/MainActivity.kt
@@ -24,6 +24,7 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
