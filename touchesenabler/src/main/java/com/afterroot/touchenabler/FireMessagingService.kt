0bc8a4a365e4902be1ce8d21f7f190ad0cb8eee5
Sandip <sandipvaghela311@gmail.com>
2019-09-19 18:48:22 +0530

    touchesenabler: cloud notifications can handle links

--- touchesenabler/src/main/java/com/afterroot/touchenabler/FireMessagingService.kt
+++ touchesenabler/src/main/java/com/afterroot/touchenabler/FireMessagingService.kt
@@ -15,55 +15,79 @@
 
 package com.afterroot.touchenabler
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FireMessagingService : FirebaseMessagingService() {
 
-    private val TAG = "FireMessagingService"
-    private val keyUrl = "FCM_URL"
+    private val _tag = "FireMessagingService"
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.e(TAG, "NEW_TOKEN $token")
-
+        Log.e(_tag, "NEW_TOKEN $token")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.from)
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.isNotEmpty()) {
-            val url = remoteMessage.data[keyUrl]
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        if (remoteMessage.notification != null && remoteMessage.data.isNotEmpty()) {
+            sendNotification(
+                message = remoteMessage.notification!!.body!!,
+                url = remoteMessage.data["link"],
+                channelId = remoteMessage.notification!!.channelId,
+                channelName = remoteMessage.data["cname"],
+                title = remoteMessage.notification?.title
+            )
         }
+    }
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
+    private fun sendNotification(
+        message: String,
+        url: String? = "",
+        channelId: String? = getString(R.string.fcm_channel_id),
+        channelName: String? = getString(R.string.fcm_channel_default),
+        title: String? = getString(R.string.app_name)
+    ) {
+        val intent: Intent
+        if (url!!.isEmpty()) {
+            intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        } else {
+            intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
+            }
         }
-
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, "ShowTouches")
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(remoteMessage.notification!!.body)
-                .setAutoCancel(false)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId ?: getString(R.string.fcm_channel_id))
+            .setSmallIcon(R.drawable.ic_splash_screen)
+            .setContentTitle(title ?: getString(R.string.app_name))
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setColor(ContextCompat.getColor(this, R.color.color_secondary))
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId ?: getString(R.string.fcm_channel_id),
+                channelName ?: getString(R.string.fcm_channel_default),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
 
         notificationManager.notify(0, notificationBuilder.build())
     }
