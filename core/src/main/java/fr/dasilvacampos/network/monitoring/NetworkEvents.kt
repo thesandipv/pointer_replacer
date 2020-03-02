ae98f0b9c2c34fb627b89451485bf8fe4b4bb3db
Sandip Vaghela <sandipvaghela311@gmail.com>
2020-03-02 23:18:31 +0530

    Add network monitoring

---
+++ core/src/main/java/fr/dasilvacampos/network/monitoring/NetworkEvents.kt
@@ -0,0 +1,15 @@
+package fr.dasilvacampos.network.monitoring
+
+import androidx.lifecycle.LiveData
+
+/**
+ * This liveData enabling network connectivity monitoring
+ * @see NetworkStateHolder to get current connection state
+ */
+object NetworkEvents : LiveData<Event>() {
+
+    internal fun notify(event: Event) {
+        postValue(event)
+    }
+
+}
