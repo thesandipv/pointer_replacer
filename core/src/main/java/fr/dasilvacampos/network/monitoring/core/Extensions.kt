f86f92b2614d810aa318ba3172732ea092b3e9ff
Sandip <sandipvaghela311@gmail.com>
2020-03-07 11:34:39 +0530

    removed document injection

--- core/src/main/java/fr/dasilvacampos/network/monitoring/core/Extensions.kt
+++ core/src/main/java/fr/dasilvacampos/network/monitoring/core/Extensions.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016-2019 Sandip Vaghela
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.dasilvacampos.network.monitoring.core
 
 import android.app.Activity
@@ -45,20 +60,20 @@ internal fun NetworkConnectivityListener.onListenerResume(networkState: NetworkS
  */
 internal var NetworkConnectivityListener.previousState: Boolean?
     get() {
-        return when {
-            this is Fragment -> this.arguments?.previousState
-            this is Activity -> this.intent.extras?.previousState
+        return when (this) {
+            is Fragment -> this.arguments?.previousState
+            is Activity -> this.intent.extras?.previousState
             else -> null
         }
     }
     set(value) {
-        when {
-            this is Fragment -> {
+        when (this) {
+            is Fragment -> {
                 val a = this.arguments ?: Bundle()
                 a.previousState = value
                 this.arguments = a
             }
-            this is Activity -> {
+            is Activity -> {
                 val a = this.intent.extras ?: Bundle()
                 a.previousState = value
                 this.intent.replaceExtras(a)
