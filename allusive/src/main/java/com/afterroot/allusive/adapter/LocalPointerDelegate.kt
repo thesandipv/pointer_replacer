e62a0061f7cbed3d0598d63cacd70c9bd0a41c2b
Sandip <sandipvaghela311@gmail.com>
2020-03-09 11:18:20 +0530

    fix crash

--- allusive/src/main/java/com/afterroot/allusive/adapter/LocalPointerDelegate.kt
+++ allusive/src/main/java/com/afterroot/allusive/adapter/LocalPointerDelegate.kt
@@ -19,10 +19,12 @@ import android.content.Context
 import android.os.Build
 import android.os.Environment
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
 import androidx.recyclerview.widget.RecyclerView
 import com.afterroot.allusive.GlideApp
 import com.afterroot.allusive.R
+import com.afterroot.allusive.Settings
 import com.afterroot.allusive.adapter.callback.ItemSelectedCallback
 import com.afterroot.allusive.getMinPointerSize
 import com.afterroot.allusive.model.IPointer
@@ -34,7 +36,7 @@ import kotlinx.android.synthetic.main.item_pointer_repo.view.*
 import org.koin.core.Koin
 
 class LocalPointerDelegate(val callbacks: ItemSelectedCallback, koin: Koin) : TypeDelegateAdapter {
-    val doc = koin.get<DocumentFile>()
+    val settings = koin.get<Settings>()
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = PointerVH(parent)
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: IPointer) {
@@ -42,23 +44,25 @@ class LocalPointerDelegate(val callbacks: ItemSelectedCallback, koin: Koin) : Ty
         holder.bind(item as RoomPointer)
     }
 
-
     inner class PointerVH(parent: ViewGroup) : RecyclerView.ViewHolder(parent.inflate(R.layout.item_pointer_repo)) {
         val context: Context = parent.context
 
         fun bind(pointer: RoomPointer) {
             itemView.info_pointer_pack_name.text = pointer.pointer_name
             itemView.info_username.text =
-                    String.format(context.getString(R.string.str_format_uploaded_by), pointer.uploader_name)
+                String.format(context.getString(R.string.str_format_uploaded_by), pointer.uploader_name)
             itemView.info_pointer_image.apply {
                 onVersionGreaterThanEqualTo(Build.VERSION_CODES.LOLLIPOP, {
-                    GlideApp.with(context).load(doc.findFile(pointer.file_name)?.uri)
-                            .override(context.getMinPointerSize(), context.getMinPointerSize())
-                            .into(this)
+                    GlideApp.with(context).load(
+                            DocumentFile.fromTreeUri(context, settings.safUri?.toUri()!!)?.findFile(pointer.file_name)?.uri
+                        )
+                        .override(context.getMinPointerSize(), context.getMinPointerSize())
+                        .into(this)
                 }, {
-                    GlideApp.with(context).load("${Environment.getExternalStorageDirectory()}${context.getString(R.string.pointer_folder_path)}${pointer.file_name}")
-                            .override(context.getMinPointerSize(), context.getMinPointerSize())
-                            .into(this)
+                    GlideApp.with(context)
+                        .load("${Environment.getExternalStorageDirectory()}${context.getString(R.string.pointer_folder_path)}${pointer.file_name}")
+                        .override(context.getMinPointerSize(), context.getMinPointerSize())
+                        .into(this)
                 })
                 background = context.getDrawableExt(R.drawable.transparent_grid)
             }
