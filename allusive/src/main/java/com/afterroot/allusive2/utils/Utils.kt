53832bcb47982c8abb52f7c6ee2ee32ad4d3f8f9
Sandip Vaghela <sandipvaghela311@gmail.com>
2020-11-14 00:55:46 +0530

    Network Monitor and Fix start issues on some launcher

---
+++ allusive/src/main/java/com/afterroot/allusive2/utils/Utils.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016-2020 Sandip Vaghela
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.afterroot.allusive2.utils
+
+import android.content.Context
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afterroot.allusive2.R
+import com.afterroot.allusive2.network.NetworkState
+
+/**
+ * Top Level Misc Functions
+ * */
+
+fun Context.showNetworkDialog(
+    state: NetworkState,
+    positive: () -> Unit,
+    negative: () -> Unit,
+    isShowHide: Boolean = false
+) =
+    MaterialDialog(this).show {
+        title(text = if (state == NetworkState.CONNECTION_LOST) "Connection Lost" else "Network Disconnected")
+        cancelable(false)
+        message(R.string.dialog_msg_no_network)
+        negativeButton(text = "Exit") {
+            negative()
+        }
+        positiveButton(text = "Retry") {
+            positive()
+        }
+        if (isShowHide) {
+            positiveButton(text = "Hide") {
+                dismiss()
+            }
+        }
+    }
