757555511c207d1303c5afb9507a52de2d9a3320
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2022-02-19 14:12:16 +0530

    encrypt google services

--- release/decrypt.sh
+++ release/decrypt.sh
@@ -2,3 +2,4 @@
 
 gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output release/keystore.jks release/keystore.gpg
 gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output release/serviceAccount.json release/serviceAccount.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=${ENCRYPT_KEY} --output allusive/google-services.json allusive/google-services.gpg
