62422735f0a30717e99d8854401780fe7bfd14ce
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2022-07-05 20:29:39 +0530

    fix crashes related to themes

--- ui/common-compose/src/main/java/com/afterroot/ui/common/compose/theme/Theme3.kt
+++ ui/common-compose/src/main/java/com/afterroot/ui/common/compose/theme/Theme3.kt
@@ -15,11 +15,31 @@
 package com.afterroot.ui.common.compose.theme
 
 import android.content.Context
+import android.os.Build
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import com.afterroot.allusive2.Settings
+import com.afterroot.allusive2.resources.R
 
 @Composable
 fun Theme(context: Context, content: @Composable () -> Unit) {
-    MaterialTheme(colorScheme = dynamicDarkColorScheme(context), typography = MaterialTheme.typography, content = content)
+    val settings = Settings(context)
+    val colorScheme: ColorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        when (settings.theme) {
+            context.getString(R.string.theme_light) -> dynamicLightColorScheme(context)
+            else -> dynamicDarkColorScheme(context)
+        }
+    } else {
+        when (settings.theme) {
+            context.getString(R.string.theme_light) -> lightColorScheme()
+            else -> darkColorScheme()
+        }
+    }
+
+    MaterialTheme(colorScheme = colorScheme, typography = MaterialTheme.typography, content = content)
 }
