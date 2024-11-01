c3507ddcd07388b148ebbf606dda38ad04966aff
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2024-11-01 18:39:33 +0530

    refactor deprecations

--- data/src/main/java/com/afterroot/allusive2/di/FirebaseModule.kt
+++ data/src/main/java/com/afterroot/allusive2/di/FirebaseModule.kt
@@ -16,23 +16,23 @@ package com.afterroot.allusive2.di
 
 import com.afterroot.allusive2.utils.whenBuildIs
 import com.afterroot.data.utils.utils
+import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.analytics
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.auth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
+import com.google.firebase.database.database
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.firestoreSettings
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.PersistentCacheSettings
+import com.google.firebase.firestore.firestore
 import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.ktx.messaging
+import com.google.firebase.messaging.messaging
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -45,8 +45,8 @@ object FirebaseModule {
   @Provides
   @Singleton
   fun provideFirestore(): FirebaseFirestore = Firebase.firestore.apply {
-    firestoreSettings = firestoreSettings {
-      isPersistenceEnabled = true
+    firestoreSettings = com.google.firebase.firestore.firestoreSettings {
+      setLocalCacheSettings(PersistentCacheSettings.newBuilder().build())
     }
   }
 
