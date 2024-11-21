d0d3d67b1ea3b01ff468eec77808387c10873619
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2024-11-21 23:54:17 +0530

    add explicit dependency to Lint Tasks for createEmptyModuleZip task

--- ui/magisk/build.gradle.kts
+++ ui/magisk/build.gradle.kts
@@ -1,17 +1,5 @@
-/*
- * Copyright (C) 2016-2023 Sandip Vaghela
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import com.android.build.gradle.internal.lint.AndroidLintAnalysisTask
+import com.android.build.gradle.internal.lint.LintModelWriterTask
 
 plugins {
   id("com.afterroot.android.library")
@@ -54,4 +42,10 @@ dependencies {
   implementation(libs.libsu.io)
 
   implementation("net.lingala.zip4j:zip4j:2.11.5")
+}
+
+tasks.whenTaskAdded {
+  if (this is LintModelWriterTask || this is AndroidLintAnalysisTask) {
+    this.mustRunAfter("createEmptyModuleZip")
+  }
 }
